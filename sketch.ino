86840ab507d8ffe889606fdf0737d0183d215a3a
Max Nargang <CoffeeCoder1@outlook.com>
2024-08-31 21:46:50 -0400

    Add initial display support

--- sketch.ino
+++ sketch.ino
@@ -1,10 +1,33 @@
 // Now turn this trash into treasure!
+#include <Wire.h>
+#include <SSD1306Ascii.h>
+#include <SSD1306AsciiWire.h>
+
+// 0X3C+SA0 - 0x3C or 0x3D
+#define DISPLAY_I2C_ADDRESS 0x3C
+#define OLED_RST_PIN -1
+
+// Declare an object for the OLED
+SSD1306AsciiWire oled;
 
 void setup() {
   // put your setup code here, to run once:
   Serial1.begin(115200);
-  Serial1.println("Hello, Raspberry Pi Pico W!");
+
+  Wire.begin();
+  Wire.setClock(400000L);
+
+#if OLED_RST_PIN >= 0
+  oled.begin(&Adafruit128x64, DISPLAY_I2C_ADDRESS, OLED_RST_PIN);
+#else // OLED_RST_PIN >= 0
+  oled.begin(&Adafruit128x64, DISPLAY_I2C_ADDRESS);
+#endif // OLED_RST_PIN >= 0
+
+  oled.setFont(System5x7);
+  oled.clear();
+  oled.println("Initializing...");
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
   delay(1); // this speeds up the simulation
